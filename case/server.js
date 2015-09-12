e3ec99cb9a70d54ff36845b073fb80e61647b962
Bogdan Chadkin <trysound@yandex.ru>
2015-09-12 13:00:55 +0300

    Replaced open with opn

--- case/server.js
+++ case/server.js
@@ -1,18 +1,19 @@
 var connect = require('connect');
-var open = require('open');
+var open = require('opn');
 var serveStatic = require('serve-static');
 var log = require('gulp-util/lib/log');
 var chalk = require('chalk');
 
-module.exports = function (dir, port, shouldOpen) {
+module.exports = function (dir, port, opener) {
 	var host = 'http://localhost:' + port;
+	var opts = typeof opener === 'string' ? { app: opener } : undefined;
 
 	return new Promise(function (resolve) {
 		connect()
 			.use(serveStatic(dir))
 			.listen(port, function () {
-				if (shouldOpen) {
-					open(host);
+				if (opener) {
+					open(host, opts);
 				}
 				log('Started server on ' + chalk.cyan(host));
 				resolve();
